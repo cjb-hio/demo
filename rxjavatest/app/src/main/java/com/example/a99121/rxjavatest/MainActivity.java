878519c5d9f53864b8bcbb085beeb5bc50a5eb0b
cjb <991217655@qq.com>
2019-04-22 22:07:27 +0800

    1

--- rxjavatest/app/src/main/java/com/example/a99121/rxjavatest/MainActivity.java
+++ rxjavatest/app/src/main/java/com/example/a99121/rxjavatest/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.a99121.rxjavatest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Observable.just(1,2,3,4).subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.i("test","value:"+integer);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
     }
 }
