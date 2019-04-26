013828095ff20f6c0534a04213b1e485507a9e56
cjb <991217655@qq.com>
2019-04-26 16:58:43 +0800

    11

--- MyGlide/app/src/main/java/com/example/cjb/myglide/glide/cache/recycle/BitmapPool.java
+++
@@ -1,8 +0,0 @@
-package com.example.cjb.myglide.glide.cache.recycle;
-
-import android.graphics.Bitmap;
-
-public interface BitmapPool {
-    void put(Bitmap bitmap);
-    Bitmap get(int width,int height,Bitmap.Config config);
-}
